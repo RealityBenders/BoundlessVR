95f798483d798aadcdc62e4436339de17b9ae82a
shreyaaa-s <ssasmal7@gatech.edu>
2025-09-27 15:16:30 -0400

    ui trigger when object is grabbed

--- BoundlessVRDemo/BoundlessVrDemo/Assets/Scripts/GrabbableTrigger.cs
+++ BoundlessVRDemo/BoundlessVrDemo/Assets/Scripts/GrabbableTrigger.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using TMPro;  // only if using TextMeshPro
+
 
 [RequireComponent(typeof(XRGrabInteractable))]
-public class GrabbableScore : MonoBehaviour
+public class GrabbableTrigger : MonoBehaviour
 {
     private XRGrabInteractable grabInteractable;
+
+    [SerializeField] private GameObject uiPanel; // assign in Inspector
+    [SerializeField] private TMP_Text uiText;    // optional: assign TMP text field
 
     private void Awake()
     {
@@ -25,5 +30,14 @@ public class GrabbableScore : MonoBehaviour
     private void OnGrab(SelectEnterEventArgs args)
     {
         ScoreManager.Instance.AddPoint();
+        // Show UI panel
+        if (uiPanel != null)
+            uiPanel.SetActive(true);
+
+        // Update UI text
+        if (uiText != null)
+            uiText.text = "You grabbed: " + gameObject.name;
+
+
     }
 }
