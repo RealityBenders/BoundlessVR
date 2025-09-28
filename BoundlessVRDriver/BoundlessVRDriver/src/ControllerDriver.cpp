e28828a2045b70e826d55e5483f1241be87b5e3b
Carson Ray <carsonray314@gmail.com>
2025-09-27 20:36:32 -0400

    Added step counting logging to driver

--- BoundlessVRDriver/BoundlessVRDriver/src/ControllerDriver.cpp
+++ BoundlessVRDriver/BoundlessVRDriver/src/ControllerDriver.cpp
@@ -241,13 +241,20 @@ void ControllerDriver::firmwareReadHandler(std::shared_ptr<MinBiTCore> protocol,
         case IMU_QUAT: {
             // Send acknowledge
             protocol->writeByte(ACK);
+            protocol->sendAll();
             // Process IMU data
+            // Reads in quaterniond
+            protocol->readQuaterniond();
             break;
         }
         case IMU_STEP: {
             // Send acknowledge
             protocol->writeByte(ACK);
+            protocol->sendAll();
             // Process IMU step data
+            // Reads in step timestamp
+            uint64_t stepTime = protocol->readData<uint64_t>();
+            VRDriverLog()->Log(("Step: " + std::to_string(stepTime)).c_str());
 			break;
         }
         default:
